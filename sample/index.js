db592d66dee55b02c85527df344c83eb57f59b71
Julian R. Smith <jrs@codesmith.co>
2021-06-16 11:11:47 -0500

    Add new endpoints

--- sample/index.js
+++ sample/index.js
@@ -16,7 +16,7 @@ TastyWorks.authorization()
     .then(token => {
         // Set the authorization in the headers
         TastyWorks.setAuthorizationToken(token);
-        console.log('Session is active, continue with other calls.');
+        //console.log('// Session is active, continue with other calls.');
         return true;
     })
     .then(() => TastyWorks.accounts())
@@ -45,23 +45,68 @@ TastyWorks.authorization()
     .catch(err => console.log(err.status))
     .then(liveOrders => {
         console.log('======= ACCOUNT LIVEORDERS =======');
-        console.log(liveOrders)
+        console.log(JSON.stringify(liveOrders, null, 4))
     })
-    .then(() => TastyWorks.history(TASTY_ACCOUNT_ID, '01/01/2019', '01/05/2019'))
+    .then(() => TastyWorks.history(TASTY_ACCOUNT_ID, '27/11/2020', '27/11/2020'))
     .catch(err => console.log(err.status))
     .then(history => {
         console.log('======= ACCOUNT HISTORY =======');
         console.log(history)
     })
     .then(() => TastyWorks.marketMetrics(['AMZN', 'SPX']))
+    .catch(err => console.log(err.status))
     .then(marketData => {
         console.log('======= Market Data =======');
-        console.log(marketData)
+        console.log(JSON.stringify(marketData, null, 4))
     })
     .then(() => TastyWorks.optionChain('TSLA'))
+    .catch(err => console.log(err.status))
     .then(chain => {
         console.log('======= Option chain =======');
         console.log(chain)
         console.table(chain.items[0].expirations)
     })
+    .then(() => TastyWorks.watchlists())
+    .catch(err => console.log(err.status))
+    .then(watchlists => {
+        console.log('======= (Private) Watchlists =======');
+        console.log(watchlists)
+    })
+    .then(() => TastyWorks.publicWatchlists())
+    .catch(err => console.log(err.status))
+    .then(publicWatchlists => {
+        console.log('======= (Public) Watchlists =======');
+        console.log(publicWatchlists)
+    })
+    .then(() => TastyWorks.margin(TASTY_ACCOUNT_ID))
+    .catch(err => console.log(err.status))
+    .then(margin => {
+        console.log('======= Margin =======');
+        console.log(JSON.stringify(margin, null, 4))
+    })
+    .then(() => TastyWorks.tradingStatus(TASTY_ACCOUNT_ID))
+    .catch(err => console.log(err.status))
+    .then(tradingStatus => {
+        console.log('======= Trading Status =======');
+        console.log(tradingStatus)
+    })
+    .then(() => TastyWorks.futures())
+    .catch(err => console.log(err.status))
+    .then(futures => {
+        console.log('======= Futures =======');
+        console.log(JSON.stringify(futures, null, 4))
+    })
+    .then(() => TastyWorks.decimals())
+    .catch(err => console.log(err.status))
+    .then(decimals => {
+        console.log('======= Decimals =======');
+        console.log(decimals)
+    })
+    .then(() => TastyWorks.cryptos())
+    .catch(err => console.log(err.status))
+    .then(cryptos => {
+        console.log('======= Cryptos =======');
+        console.log(cryptos)
+    })
+
 
--- util/endpoints.js
+++ util/endpoints.js
@@ -2,17 +2,31 @@ const baseURL = 'https://api.tastyworks.com';
 //trade.dough.com
 
 module.exports = {
+    // Authentication
     login: () => `${baseURL}/sessions`,
     validateLogin: () => `${baseURL}/sessions/validate`,
+    streamer: () => `${baseURL}/quote-streamer-tokens`,
+    // Account specific
     accounts: () => `${baseURL}/customers/me/accounts`,
     balances: (account_id) => `${baseURL}/accounts/${account_id}/balances`,
     positions: (account_id) => `${baseURL}/accounts/${account_id}/positions`,
     liveOrders: (account_id) => `${baseURL}/accounts/${account_id}/orders/live`,
     executeOrder: (account_id) => `${baseURL}/accounts/${account_id}/orders`,
     cancelOrder: (account_id, order_id) => `${baseURL}/accounts/${account_id}/orders/${order_id}`,
-    streamer: () => `${baseURL}/quote-streamer-tokens`,
   	optionChain: (ticker) => `${baseURL}/option-chains/${ticker}/nested`,
     history: (account_id) => `${baseURL}/accounts/${account_id}/transactions`,
-    marketMetrics: (tickers) => `${baseURL}/market-metrics?symbols=${tickers}`
+    marketMetrics: (tickers) => `${baseURL}/market-metrics?symbols=${tickers}`,
+    watchlists: () => `${baseURL}/watchlists`,
+    publicWatchlists: () => `${baseURL}/public-watchlists`,
+    tradingStatus: (account_id) => `${baseURL}/accounts/${account_id}/trading-status`,
+    margin: (account_id) => `${baseURL}/margin/accounts/${account_id}`,
+    // Generic (non-account specific)
+    futures: () => `${baseURL}/instruments/futures`,
+    decimals: () => `${baseURL}/instruments/quantity-decimal-precisions`,
+    cryptos: () => `${baseURL}/instruments/cryptocurrencies`
+
     // TODO: '{url}/dry-run'
+    // https://api.tastyworks.com/position-groups/A0001635099
+    // /traders
+    // /public_orders
 }
